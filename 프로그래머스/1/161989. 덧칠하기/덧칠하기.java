1686fe4c2ae78de0546e66a2a163e40da1f1f0e2
changell <89053912+changell@users.noreply.github.com>
2024-08-22 16:17:43 +0900

    [level 1] Title: 덧칠하기, Time: 1.42 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,16 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 1;
-        int result = section[0] + m;    //처음 페인트칠 했을때 길이 값 체크
+        int currentEnd = section[0] + m;    //처음 페인트칠 했을때 길이 값 체크
         
         for(int i = 0; i< section.length; i++){
-            int cnt = section[i];
+            int currentSection = section[i];
             //if 페인트 칠 길이 > section 순회 값 비교
-            if(result > cnt){
-                continue;
-            }else{
-                result = cnt + m;   //페인트 칠 길이 값 최신화
-                answer += 1;        //페인트 칠한 수 + 1
-            }
+            if(currentEnd > currentSection) continue;
+            
+            currentEnd = currentSection + m;   //페인트 칠 길이 값 최신화
+            answer ++;        //페인트 칠한 수 + 1
+            
         }
         
         return answer;

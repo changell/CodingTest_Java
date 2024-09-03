ed3bf38932d91288f01e66e1bef014993e71828a
changell <89053912+changell@users.noreply.github.com>
2024-09-03 18:22:05 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 10.02 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -5,8 +5,8 @@ class Solution {
         int pLeng = p.length();
 
         // p의 길이와 동일한 길이의 부분 문자열을 만들어야 하므로 right를 pLeng-1로 설정
-        for (int left = 0; left <= tLeng - pLeng; left++) {
-            String result = t.substring(left, left + pLeng); // p의 길이만큼 부분 문자열 생성
+        for (int i = 0; i <= tLeng - pLeng; i++) {
+            String result = t.substring(i, i + pLeng); // p의 길이만큼 부분 문자열 생성
             
             long tNum = Long.parseLong(result); // 길이가 클 수 있으니 long으로 변환
             long pNum = Long.parseLong(p);
